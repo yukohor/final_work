81793e700d15e48f7e370f38d33691c72d0382da
yukohor <ka91001.xxx@yahoo.co.jp>
2024-09-19 20:38:50 +0900

    9/19

---
+++ db/mysql_setup.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS daily_report_system CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE USER IF NOT EXISTS 'repuser'@'%' IDENTIFIED BY 'reppass';
+GRANT ALL PRIVILEGES ON *.* TO 'repuser'@'%';
